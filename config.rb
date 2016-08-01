2435cebdb0aea0f4d56312b876457f214b172e3f
AlexandreSaintJean <xelastjean@gmail.com>
2016-08-01 17:28:52 +0200

    my new dynamic page

--- config.rb
+++ config.rb
@@ -27,9 +27,13 @@ activate :autoprefixer
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# Proxy pages (http://middlemanapp.com/flats/show.html.erb/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+data.flats.each do |name, options|
+  proxy "flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }
+
+end
 
 ###
 # Helpers
